445e75b0c0d697b697a9a638a4ca799331ba0cc2
Fene-87 <fenekayas87@gmail.com>
2022-12-19 10:55:00 -0900

    Fix linters errors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const navList = document.querySelector('.nav-list');
 const navIcon = document.querySelector('.nav-icon-span');
-const removeList = document.querySelector('.nav-list-remove')
+const removeList = document.querySelector('.nav-list-remove');
 
-navIcon.addEventListener("click", () => {
-    navList.classList.toggle("active")
-})
+navIcon.addEventListener('click', () => {
+  navList.classList.toggle('active');
+});
 
-removeList.addEventListener("click", () => {
-    navList.classList.remove("active")
-})
+removeList.addEventListener('click', () => {
+  navList.classList.remove('active');
+});
