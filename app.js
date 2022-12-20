b4c775de77c9591d34e4f48e67c78a9f1cfb206e
Fene-87 <fenekayas87@gmail.com>
2022-12-19 23:02:19 -0900

    Add event listener to each nav link

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const navList = document.querySelector('.nav-list');
 const navIcon = document.querySelector('.nav-icon-span');
 const removeList = document.querySelector('.nav-list-remove');
+const removeSlider = document.querySelectorAll('.nav-li');
 
 navIcon.addEventListener('click', () => {
   navList.classList.toggle('active');
@@ -8,4 +9,10 @@ navIcon.addEventListener('click', () => {
 
 removeList.addEventListener('click', () => {
   navList.classList.remove('active');
+});
+
+removeSlider.forEach((item) => {
+  item.addEventListener('click', () => {
+    navList.classList.remove('active');
+  });
 });
